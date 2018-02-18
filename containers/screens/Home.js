664479f590f674e523610507372347bfd26c60f4
Jocélio Lima <=jocelio27@gmail.com>
2018-02-18 01:34:51 -0300

    Singout button.

--- containers/screens/Home.js
+++ containers/screens/Home.js
@@ -1,12 +1,12 @@
 /**
  * Created by jocelio on 14/02/18.
  */
-
 import React, {Component} from "react";
 import {Text} from "react-native";
 import {Button, Container, Content} from 'native-base'
 import MenuSettings from "../common/MenuSettings";
 import CustomHeader from '../common/CustomHeader'
+import {onSignOut} from "../../auth/auth";
 
 export default class Home extends Component {
 
@@ -22,8 +22,9 @@ export default class Home extends Component {
 
                     <Text>Home Screen</Text>
 
-                    <Button onPress={() => this.props.navigation.navigate('Login')} full>
-                        <Text style={{ color: 'white' }}>Go To Login Screen</Text>
+                    <Button onPress={() => onSignOut().then(() => this.props.navigation.navigate("SignedOut"))}
+                    full>
+                        <Text style={{ color: 'white' }}>Logout</Text>
                     </Button>
                 </Content>
 
